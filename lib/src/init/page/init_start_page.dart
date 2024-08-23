daab5e5a9b2cdab10e083906f6c21192fa192671
sangjun <byjun0302@gmail.com>
2024-08-23 15:13:06 +0900

    feat: make InitStartPage UI

--- lib/src/init/page/init_start_page.dart
+++ lib/src/init/page/init_start_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
+import 'package:sanrio_market/src/common/components/app_font.dart';
 
 class InitStartPage extends StatelessWidget {
   const InitStartPage({super.key});
@@ -16,25 +17,44 @@ class InitStartPage extends StatelessWidget {
               height: 116,
               child: Image.asset('assets/images/pompom.webp'),
             ),
-            const SizedBox(height: 40,),
-            Text(
-              '당신 근처의 산리오 마켓',
-              style: GoogleFonts.notoSans(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.white,
-              ),
+            const SizedBox(
+              height: 40,
             ),
-            const SizedBox(height: 15,),
-            Text(
+            const AppFont(
+              '당신 근처의 산리오 마켓',
+              fontWeight: FontWeight.bold,
+              size: 20,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            AppFont(
               '국내 및 해외 산리오 제품구매까지, \n 지금 내 동네를 선택하고 시작해보세요!',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.notoSans(
-                fontSize: 18,
-                color: Colors.white.withOpacity(0.6),
-              ),
+              align: TextAlign.center,
+              size: 18,
+              color: Colors.white.withOpacity(0.6),
             ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(
+            left: 25, right: 25, bottom: 25 + Get.mediaQuery.padding.bottom),
+        child: GestureDetector(
+          onTap: () {},
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(7),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              color: Colors.pink,
+              child: const AppFont(
+                '시작하기',
+                align: TextAlign.center,
+                size: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
