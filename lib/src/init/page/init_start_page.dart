41a92d1833cbf5419b6f482d6ef1b5559f16c49b
sangjun <byjun0302@gmail.com>
2024-08-23 15:00:20 +0900

    feat: add app_font to project

--- lib/src/init/page/init_start_page.dart
+++ lib/src/init/page/init_start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class InitStartPage extends StatelessWidget {
   const InitStartPage({super.key});
@@ -7,9 +8,33 @@ class InitStartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          '초기 소개 페이지',
-          style: TextStyle(color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 99,
+              height: 116,
+              child: Image.asset('assets/images/pompom.webp'),
+            ),
+            const SizedBox(height: 40,),
+            Text(
+              '당신 근처의 산리오 마켓',
+              style: GoogleFonts.notoSans(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 15,),
+            Text(
+              '국내 및 해외 산리오 제품구매까지, \n 지금 내 동네를 선택하고 시작해보세요!',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.notoSans(
+                fontSize: 18,
+                color: Colors.white.withOpacity(0.6),
+              ),
+            ),
+          ],
         ),
       ),
     );
